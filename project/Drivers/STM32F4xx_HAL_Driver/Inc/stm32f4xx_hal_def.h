bd527f10bff2bc2e0839b14083e62faf1c6f2d3e
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2019-04-03 11:49:30 -0600

    Fix broken header file from STM32Cube.
    By default the HAL library uses HAL_StatusTypeDef and HAL_LockTypeDef
    before they are defined.

--- project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h
+++ project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h
@@ -42,6 +42,28 @@
  extern "C" {
 #endif
 
+
+/**
+  * @brief  HAL Status structures definition
+  */
+typedef enum
+{
+    HAL_OK       = 0x00U,
+    HAL_ERROR    = 0x01U,
+    HAL_BUSY     = 0x02U,
+    HAL_TIMEOUT  = 0x03U
+} HAL_StatusTypeDef;
+
+/**
+  * @brief  HAL Lock structures definition
+  */
+typedef enum
+{
+    HAL_UNLOCKED = 0x00U,
+    HAL_LOCKED   = 0x01U
+} HAL_LockTypeDef;
+
+
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f4xx.h"
 #include "Legacy/stm32_hal_legacy.h"
@@ -49,25 +71,6 @@
 
 /* Exported types ------------------------------------------------------------*/
 
-/** 
-  * @brief  HAL Status structures definition  
-  */  
-typedef enum 
-{
-  HAL_OK       = 0x00U,
-  HAL_ERROR    = 0x01U,
-  HAL_BUSY     = 0x02U,
-  HAL_TIMEOUT  = 0x03U
-} HAL_StatusTypeDef;
-
-/** 
-  * @brief  HAL Lock structures definition  
-  */
-typedef enum 
-{
-  HAL_UNLOCKED = 0x00U,
-  HAL_LOCKED   = 0x01U  
-} HAL_LockTypeDef;
 
 /* Exported macro ------------------------------------------------------------*/
 
